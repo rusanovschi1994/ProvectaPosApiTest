c6a9ac77b109455de0503f984ac00f707d5221b9
RUSANOVSCHI <rusanovschi1994@gmail.com>
2023-02-01 14:39:36 +0200

    add BranchApiTest.java

---
+++ src/test/java/provecta_api_tests/BranchApiTest.java
@@ -0,0 +1,35 @@
+package provecta_api_tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import provecta_api_tests.base.LoginApiTest;
+import provecta_api_tests.base.Specification;
+import provecta_pos.pojo.get.branch_controller.Branch;
+import provecta_pos.read_properties.ConfigProvider;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static io.restassured.RestAssured.given;
+
+public class BranchApiTest extends LoginApiTest {
+
+    @Test
+    public void checkBranchNameWithNotNullValue(){
+
+        Specification.installSpecification(Specification.requestSpec(ConfigProvider.BASE_URL), Specification.responseSpecOK200());
+
+        List<Branch> branch = given()
+                .header("Authorization", getLoginTypeAndToken())
+                .when()
+                .get("api/v1/framework/owner/branch/select")
+                .then().log().body()
+                .extract().jsonPath().getList(".", Branch.class);
+
+        List<String> branches = branch.stream().map(Branch::getName).collect(Collectors.toList());
+        for(String name : branches){
+
+            Assert.assertNotNull("some branch name have a blank or null value", name);
+        }
+    }
+}
